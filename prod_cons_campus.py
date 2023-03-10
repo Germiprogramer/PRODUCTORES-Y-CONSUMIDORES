fa7113ad1ea7cf08065d9892677d7c6cea394f00
Germiprogramer <llorentemunozgerman@gmail.com>
2023-03-10 16:02:46 +0100

    Update prod_cons_campus.py

--- prod_cons_campus.py
+++ prod_cons_campus.py
@@ -1,18 +1,20 @@
 from queue import Queue
 
-from threading import Thread
-
 import threading
+
+from threading import Thread, Timer, Semaphore
 
 import time
 
-# Crear cola
-
 num_bollos_inicial = int(input("Introduce el numero de bollos: "))
+
+# queue que simula el mostrador
 
 q = Queue(maxsize=num_bollos_inicial)
 
-e = threading.Semaphore(num_bollos_inicial)
+# semaforo para que los consumidores no consuman el mismo bollo
+
+e = Semaphore(num_bollos_inicial)
 
 def bollos_iniciales(num_bollos_inicial):
 
@@ -21,8 +23,6 @@ def bollos_iniciales(num_bollos_inicial):
     for i in range(num_bollos_inicial-1):
 
         q.put(i+1)
-
-    print(q.queue)
 
 bollos_iniciales(num_bollos_inicial)
 
@@ -34,8 +34,6 @@ def producer(name):
 
     while True:
 
-        # Espera a task_done () para enviar una señal
-
         q.put(count)
 
         e.acquire()
@@ -44,11 +42,9 @@ def producer(name):
 
         count+=1
 
-        #printear una queue
-
         print("Hay",q.qsize(),"bollos en el mostrador" + "\n")
 
-        time.sleep(2)
+        time.sleep(5)
 
 def customer(name):
 
@@ -68,7 +64,7 @@ def customer(name):
 
         # Envía una señal después de comer
 
-        time.sleep(2)
+        time.sleep(8)
 
 
 
@@ -76,9 +72,9 @@ if __name__ == '__main__':
 
     t1 = Thread(target=producer,args=("Maestro Zhang",))
 
-    t2 = Thread(target=customer,args=("Chen",))
+    t2 = Timer(2,customer,args=("Chen",))
     
-    t3 = Thread(target=customer,args=("Wang",))
+    t3 = Timer(5,customer,args=("Wang",))
 
     t1.start()
 
