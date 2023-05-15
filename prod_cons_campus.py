bbc1f9c72ef45f433a6fb5f861afdf543d7491ce
Germiprogramer <llorentemunozgerman@gmail.com>
2023-05-15 16:52:04 +0200

    commit

--- prod_cons_campus.py
+++ prod_cons_campus.py
@@ -1,86 +1,51 @@
 from queue import Queue
-
-import threading
-
 from threading import Thread, Timer, Semaphore
-
 import time
 
-num_bollos_inicial = int(input("Introduce el numero de bollos: "))
+num_bollos_inicial = int(input("Introduce el número de bollos: "))
 
-# queue que simula el mostrador
-
+# Queue que simula el mostrador
 q = Queue(maxsize=num_bollos_inicial)
 
-# semaforo para que los consumidores no consuman el mismo bollo
-
+# Semaphore para controlar el acceso a los bollos
 e = Semaphore(num_bollos_inicial)
 
 def bollos_iniciales(num_bollos_inicial):
-
-    print("Los bollos iniciales son: ", num_bollos_inicial)
-
-    for i in range(num_bollos_inicial-1):
-
+    print("Los bollos iniciales son:", num_bollos_inicial)
+    for i in range(num_bollos_inicial):
         q.put(i+1)
 
 bollos_iniciales(num_bollos_inicial)
 
 def producer(name):
-
     """Productor"""
-
-    count = 1 + num_bollos_inicial #mostrador
-
+    count = num_bollos_inicial + 1  # Contador de bollos
     while True:
-
         q.put(count)
-
         e.acquire()
-
-        print(f"{name} está produciendo el bollo {count}" + "\n")
-
-        count+=1
-
-        print("Hay",q.qsize(),"bollos en el mostrador" + "\n")
-
+        print(f"{name} está produciendo el bollo {count}")
+        count += 1
+        print(f"Hay {q.qsize()} bollos en el mostrador")
         time.sleep(4)
 
 def customer(name):
-
-    """consumidor"""
-
-    count = 1
-
+    """Consumidor"""
     while True:
-
-        baozi = q.get(block=True) # Si no hay bollos, no espera
-
-        print(f"El consumidor- {name} está comiendo el bollo {baozi}" + "\n")
-
+        baozi = q.get(block=True)  # Si no hay bollos, espera
+        print(f"El consumidor {name} está comiendo el bollo {baozi}")
         e.release()
-
-        count+=1
-
         # Envía una señal después de comer
-
         time.sleep(6)
 
-
-
 if __name__ == '__main__':
-
-    t1 = Thread(target=producer,args=("Maestro Zhang",))
-
-    t2 = Timer(2,customer,args=("Chen",))
-    
-    t3 = Timer(5,customer,args=("Wang",))
+    t1 = Thread(target=producer, args=("Maestro Zhang",))
+    t2 = Thread(target=customer, args=("Chen",))
+    t3 = Thread(target=customer, args=("Wang",))
 
     t1.start()
-
     t2.start()
-
     t3.start()
+
 
     
 
